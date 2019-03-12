e8e258dab48692992a0ab62d80ae797f9fed2ebf
Alice (UshinaUV) <alice@example.com>
2019-03-12 17:17:26 +0300

    delenie' git commit -a -m 'delenie'

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@ int main()
 int a , b ;
  cout<< "Enter A and B";
  cout<<"A + B = " << a + b << '\n';
-cout<<"A + B = " << a - b << '\n';
+cout<<"A - B = " << a - b << '\n';
 cout<<"A*B = " << a*b << '\n';
+cout<<"A/B = " << a/b << '\n';
  cin>>a>>b;
 }
